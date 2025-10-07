afe837993e3f198f1d5b91379c58b69d898aa7cf
sindhu hegde <sindhuhegde2007@gmail.com>
2025-10-07 12:21:57 +0530

    Update salarybonous.py

--- salarybonus/salarybonous.py
+++ salarybonus/salarybonous.py
@@ -4,8 +4,14 @@ def main():
         if salary < 0:
             print("❌ Error: Salary cannot be negative.")
         else:
+           bonus = salary * 0.10
+            total_salary = salary + bonus
+
             print("\n💼 Employee Salary Details")
-            print(f"   Entered Salary: ₹ {salary:,.2f}")
+            print(f"   Base Salary     : ₹ {salary:,.2f}")
+            print(f"   Bonus (10%)     : ₹ {bonus:,.2f}")
+            print(f"   Total Salary    : ₹ {total_salary:,.2f}")
+
     except ValueError:
         print("❌ Invalid input. Please enter a numeric value.")
 
